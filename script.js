46981692c02707229131357b7d1582751faf3a10
alexanderluong <alexander.luong@hotmail.com>
2018-01-30 22:53:50 -0500

    Fix cross-domain issue

--- script.js
+++ script.js
@@ -6,30 +6,30 @@ $(document).ready(function() {
   $.ajax({
     dataType: "jsonp",
     type: "GET",
-    url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30&_jsonp=mycallback",
+    jsonpCallback: "callback",
+    url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40&_jsonp=callback",
     success: function(json) {
-      console.log("json");
       quotes = json;
       randomQuote();
-    }
+    },
+    failure: function() {
+    	$("#quoteBox").html("<p>" + "Quotes could not be retrieved." + "</p>");
+	}
   });
 
   $("#quoteButton").on("click", function() {
-    index = Math.floor(Math.random() * 30);
+    index = generateIndex();
     randomQuote();
   });
-  
-  if (quotes.length == 0) {
-    $("#quoteBox").html("<p>" + "There are no quotes available right now." + "</p>");
-  }
 
   function randomQuote() {
     if (quotes.length > 0) {
-      console.log("Running random quote.");
-      console.log(quotes);
-      index = Math.floor(Math.random() * 30);
-      console.log(index);
+      index = generateIndex();
       $("#quoteBox").html("<p>" + quotes[index].content + "</p>" + "<br><p> - " + quotes[index].title + "</p>");
     } 
+  }
+
+  function generateIndex() {
+  	return Math.floor(Math.random() * 40);
   }
 });
