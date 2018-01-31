180090ad44a786132832aa7ccb94c8e53d6c9b35
alexanderluong <alexander.luong@hotmail.com>
2018-01-30 22:25:19 -0500

    Change to ajax request

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-var quoteTable = [];
-
-function randomQuote() {
-  var index = Math.floor(Math.random() * 20);
-    $("#quoteBox").html("<p>" + quoteTable[index].content + "</p>" + "<br><p> - " + quoteTable[index].title + "</p>");
-}
+var quotes = [];
+var index = 0;
 
 $(document).ready(function() {
-  $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=20", function(json) {
-    quoteTable = json;
+
+  $.ajax({
+    dataType: "jsonp",
+    type: "GET",
+    url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30&_jsonp=mycallback",
+    success: function(json) {
+      console.log("json");
+      quotes = json;
+      randomQuote();
+    }
+  });
+
+  $("#quoteButton").on("click", function() {
+    index = Math.floor(Math.random() * 30);
     randomQuote();
   });
   
-  $("#quoteButton").on("click", function() {
-    index = Math.floor(Math.random() * 15);
-    randomQuote();
-  });
+  if (quotes.length == 0) {
+    $("#quoteBox").html("<p>" + "There are no quotes available right now." + "</p>");
+  }
+
+  function randomQuote() {
+    if (quotes.length > 0) {
+      console.log("Running random quote.");
+      console.log(quotes);
+      index = Math.floor(Math.random() * 30);
+      console.log(index);
+      $("#quoteBox").html("<p>" + quotes[index].content + "</p>" + "<br><p> - " + quotes[index].title + "</p>");
+    } 
+  }
 });
